22280f6480455d59af9fe0019afe9dd70d37ea24
xinleipan <xinleipan@berkeley.edu>
2017-09-03 22:48:55 -0700

    u

--- gym_gridworld/__init__.py
+++ gym_gridworld/__init__.py
@@ -4,11 +4,3 @@ register(
     id='gridworld-v0',
     entry_point='gym_gridworld.envs:GridworldEnv',
 )
-register(
-    id='gridworld-v1',
-    entry_point='gym_gridworld.envs:GridworldEnv1',
-)
-register(
-    id='gridworld-v2',
-    entry_point='gym_gridworld.envs:GridworldEnv2',
-)
