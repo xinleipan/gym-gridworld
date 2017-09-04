34601e459f0bfd403c2499b6d1634df854be4ddd
xinleipan <xinleipan@berkeley.edu>
2017-09-03 22:52:45 -0700

    update all

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup
 
 setup(name='gym_gridworld',
       version='0.0.1',
-      install_requires=['gym',
-                        'numpy',
-                        'sys']
+      install_requires=['gym']
 )
 
