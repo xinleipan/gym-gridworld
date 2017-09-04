8eea1177bee811123f16958754ea0017b4c05a0d
harrypann <xinleipan@berkeley.edu>
2017-09-03 22:37:42 -0700

    update init

--- gym_gridworld/__init__.py
+++ gym_gridworld/__init__.py
@@ -4,3 +4,11 @@ register(
     id='gridworld-v0',
     entry_point='gym_gridworld.envs:GridworldEnv',
 )
+register(
+    id='gridworld-v1',
+    entry_point='gym_gridworld.envs:GridworldEnv1',
+)
+register(
+    id='gridworld-v2',
+    entry_point='gym_gridworld.envs:GridworldEnv2',
+)
